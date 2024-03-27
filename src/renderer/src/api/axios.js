4256a754730a0629e0516f931c9fae15cad5b786
一戬泯恩仇 <923369402@qq.com>
2024-03-28 00:36:03 +0800

    股票

--- src/renderer/src/api/axios.js
+++ src/renderer/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { stockFlag } from "@/utils/index"
 
-let prefixUrl = ""
+let prefixUrl = process.env['ELECTRON_RENDERER_URL'];
+if (process.env.NODE_ENV === 'production') {
+    prefixUrl = 'http://api.mairui.club/'
+}
 // 创建axios实例
 let instance = axios.create({
     baseURL: prefixUrl,
@@ -10,12 +13,13 @@ let instance = axios.create({
 
 // http 请求拦截器
 instance.interceptors.request.use(req => {
+    console.log(req)
     // 请求前
     req.headers["content-type"] = "application/json;charset=UTF-8"; // 默认类型
     // req.headers["Cache-Control'"] = "no-cache";
     // req.headers["Pragma'"] = "no-cache";
 
-    if(req.baseURL == "/sapi") {
+    if (req.baseURL.slice(-5) == "/sapi") {
         req.url += "?token=097c5ec77813d4077138be3288537d97364e921235175234"
     }
     return req;
@@ -39,20 +43,20 @@ instance.interceptors.response.use(req => {
  * @param {Object} params [请求时携带的参数]
  * @param {number} delay [864000000 = 10天]
  */
-export const get = (url, params, baseURL, delay = 864000000) => {
+export const get = (url, params, suffix, delay = 864000000) => {
     return new Promise((resolve, reject) => {
         instance.get(url, {
             params: params,
-            baseURL
+            baseURL: prefixUrl + suffix
         }).then(res => {
             resolve(res);
             const flag = stockFlag();
             // 判断股市是否开盘，开盘就轮询继续请求
             // 设置延迟后再次发起请求  
-            flag ? setTimeout(() => get(url, params, baseURL), delay) : null 
+            flag ? setTimeout(() => get(url, params, baseURL), delay) : null
         }).catch(err => {
             reject(err);
-            const flag = stockFlag(); 
+            const flag = stockFlag();
             flag ? setTimeout(() => get(url, params, baseURL), 1000) : null;
         })
     });
